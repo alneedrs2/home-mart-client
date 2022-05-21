cf46e177f62527f363255333198478ecfeb5fbde
Alneed Roy <alneedrs2@gmail.com>
2022-05-22 03:55:55 +0600

    navbar and banner added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import Header from "./Shared/Header/Header";
 
 function App() {
   return (
     <div>
-      <button class="btn btn-neutral">Button</button>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,46 @@
+import React from "react";
+import banner1 from "../../../Assets/banner/banner-1.png";
+import banner2 from "../../../Assets/banner/banner-2.png";
+import banner3 from "../../../Assets/banner/banner-3.png";
+
+const Banner = () => {
+  return (
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img src={banner1} className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src={banner2} className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src={banner3} className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
